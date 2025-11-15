9d7a336e5a0a7b7873783bc8a8477c3ac692ed7c
shivam17ullu <shivam17101@gmail.com>
2025-11-15 14:42:22 +0530

    minor fixes

--- src/model/auth/user.ts
+++ src/model/auth/user.ts
@@ -22,7 +22,7 @@ class User
 {
   public id!: number;
   public phone_number!: string;
-  public password!: string;
+  public password?: string;
   public is_active!: boolean;
   public readonly created_at!: Date;
   public readonly updated_at!: Date;
